8fb899318700e77af053c21ae6d5bb1eda341250
lashkovdmitry <deman19-91@yandex.ru>
2022-05-21 17:45:37 +0300

    add js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// let str = 'Любой текct';
+// const nbr = 1;
+// let bool = false;
+// let obj = {
+//     name: 'Boris',
+//     age: 20,
+//     isSilly: true,
+//     hand: {
+//         finger: 'big'
+//     }
+// };
+// let nl = null;
+// let undfined = undefined;
+// let borisName = obj.name;
+
+// function summary(first, second)
+// {
+//     return first + second;
+// }
+
+// let nbr = summary(5, 6);
+// console.log(nbr);
+
+const modal = document.querySelector('.modal');
+modal.classList.add('show');
